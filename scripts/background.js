cbce0e66255a9b2124521cc8108e4a55e8920e3c
James Schiller <schillerj78@gmail.com>
2020-08-06 22:02:44 -0400

    set-global-variables

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,8 @@
 // console.log('background js');
 
-const JM_DASHBOARD_URL = 'https://account.jinnmail.com/index.html', JM_API_URL = 'https://whatismyname2.xyz/api/v1/'; // 'https://jinnmailapp.herokuapp.com/api/v1/';
-// const JM_DASHBOARD_URL = 'https://jinnmaildashtest.herokuapp.com/index.html', JM_API_URL = 'https://jinnmailapptest.herokuapp.com/api/v1/';
-// const JM_DASHBOARD_URL = 'http://localhost:8000/index.html', JM_API_URL = 'http://localhost:3000/api/v1/';
+const JM_DASHBOARD_URL = 'https://account.jinnmail.com', JM_API_URL = 'https://whatismyname2.xyz/api/v1/'; // 'https://jinnmailapp.herokuapp.com/api/v1/';
+// const JM_DASHBOARD_URL = 'https://testling.xyz', JM_API_URL = 'https://api.testling.xyz/api/v1/';
+// const JM_DASHBOARD_URL = 'http://localhost:8000', JM_API_URL = 'http://localhost:3000/api/v1/';
 
 let url = JM_API_URL;
 
